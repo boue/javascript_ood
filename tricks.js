aff68a9384387a05b7c61298c2d65e495bcf7719
Germain Boue <boue.germain@gmail.com>
2015-11-07 15:41:14 -0800

    Add get and set functions

--- tricks.js
+++ tricks.js
@@ -1,4 +1,12 @@
 //constructor function
 function User(properties){
+  for(var i in properties){function(){
+    this[ "get" + i ] = function(){
+      return properties[i];
+    };
 
+    this[ "set" + i ] = function(val){
+      properties[i] = val;
+    };
+  })(); }
 }
